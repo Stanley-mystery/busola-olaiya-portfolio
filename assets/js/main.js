cd4ad144c4371ffe279a0fa27713889448d38a20
Stanley-mystery <stanleynwogu27@gmail.com>
2025-07-23 07:19:13 +0100

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,6 +53,23 @@
     });
   });
 
+ /**
+   * Preloader
+   */
+  const preloader = document.querySelector('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      setTimeout(() => {
+        preloader.classList.add('loaded');
+      }, 1000);
+      setTimeout(() => {
+        preloader.remove();
+      }, 2000);
+    });
+  }
+
+
+
   /**
    * Scroll top button
    */
